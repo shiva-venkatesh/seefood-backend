b7cd95608987e1ea64a8138dee37a6af760daec4
Shiva Venkatesh <shiva@bangthetable.com>
2017-05-27 00:22:18 +0530

    build endpoints for GET and POST classifications

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
-var app = express()
+var app = express();
+var bodyParser = require('body-parser')
 var Clarifai = require('clarifai')
 var secrets = require('./secrets')
 var keys = secrets.clarifaiConfig()
+
+app.use(bodyParser.json());
 
 var ClarifaiInstance = new Clarifai.App(
   keys.access_key,
@@ -15,7 +18,8 @@ var foodModel = 'bd367be194cf45149e75f01d59f77ba7'
 function getPredictionAsJSON(imageURL) {
   ClarifaiInstance.models.predict(foodModel, imageURL).then(
     function(response) {
-      store = response
+      store = response.outputs[0].data
+      console.log(store);
     },
     function(err) {
       console.error(err);
@@ -24,10 +28,15 @@ function getPredictionAsJSON(imageURL) {
 }
 
 app.listen(8000, function () {
-  getPredictionAsJSON('https://images.pexels.com/photos/66436/pexels-photo-66436.jpeg?w=940&h=650&auto=compress&cs=tinysrgb');
   console.log('Recognize app listening on port 8000!')
 
+  app.post('/classify', function(req, res) {
+    var imageURL = req.body.imageURL
+    getPredictionAsJSON(imageURL)
+    res.json((store));
+  });
+
   app.get('/classify', function(req, res) {
-    res.json((store.outputs[0]));
+    res.json((store));
   });
 })
