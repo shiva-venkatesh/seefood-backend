d280e34294cc2e32e8ca7cd5bc032cbaeca45082
Shiva Venkatesh <shiva@bangthetable.com>
2017-05-27 21:00:25 +0530

    repair commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express')
-var app = express()
+var app = express();
+var bodyParser = require('body-parser')
 var Clarifai = require('clarifai')
 var secrets = require('./secrets')
 var keys = secrets.clarifaiConfig()
+
+app.use(bodyParser.json());
 
 var ClarifaiInstance = new Clarifai.App(
   keys.access_key,
@@ -12,22 +15,33 @@ var ClarifaiInstance = new Clarifai.App(
 var store
 
 var foodModel = 'bd367be194cf45149e75f01d59f77ba7'
-function getPredictionAsJSON(imageURL) {
+var fnol = function getPredictionAsJSON(imageURL) {
   ClarifaiInstance.models.predict(foodModel, imageURL).then(
     function(response) {
-      store = response
+      store = response.outputs[0].data
+      console.log(store + ' from inside the fn');
     },
     function(err) {
       console.error(err);
     }
   );
+  return store
 }
 
 app.listen(8000, function () {
-  getPredictionAsJSON('https://images.pexels.com/photos/66436/pexels-photo-66436.jpeg?w=940&h=650&auto=compress&cs=tinysrgb');
   console.log('Recognize app listening on port 8000!')
 
+  app.post('/classify', function(req, res) {
+    var clarifaiData
+    var imageURL = req.body.imageURL
+      clarifaiData = fnol(imageURL)
+      // console.log('cldata : '+ clarifaiData)
+      var responseObject = {imageURL: imageURL, data: clarifaiData }
+      // console.log('rObj : '+responseObject)
+      res.send((responseObject));
+  });
+
   app.get('/classify', function(req, res) {
-    res.json((store.outputs[0]));
+    res.json((store));
   });
 })
